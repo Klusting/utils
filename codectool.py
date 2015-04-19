03329cac58b419acbec33d606ebf947c06560b14
Klusting <lhpzwd@163.com>
2015-04-20 01:47:36 +0800

    add output in one line

--- codectool.py
+++ codectool.py
@@ -2,9 +2,12 @@
 
 import sys
 
+a=bytes()
 string=sys.argv[1]
 for ch in string:
     print(ch)
     print(ch.encode('utf8'))
+    a+=ch.encode('utf8')
     for i in ch.encode('utf8'):
         print(oct(i))
+print(a)
